ddb7cb77298331575b5d5047ac1a623815c6e31b
Neko <karimovanatalia89@gmail.com>
2025-04-23 11:44:07 +0300

    Reformat commit

--- src/main/java/ru/netology/services/VacationCalculator.java
+++ src/main/java/ru/netology/services/VacationCalculator.java
@@ -5,14 +5,14 @@ public class VacationCalculator {
         int money = 0; // Начальное состояние счета
         int count = 0; // Счетчик месяцев отдыха
 
-        for (int month = 1; month < 12; month++) {
+        for (int month = 1; month < 13; month++) {
             if (money >= threshold) { // Проверяем количество денег для отдыха
                 count++; // Увеличиваем счетчик месяцев отдыха
-                money -= expenses; // Траты на отдых
-                money -= money / 3; // Оставляем на отдых 1/3 оставшихся денег
+                money = money - expenses; // Траты на отдых
+                money = money / 3; // Оставляем на отдых 1/3 оставшихся денег
             } else {
-                money += income; // Заработок
-                money -= expenses; // Траты
+                money = money + income; // Заработок
+                money = money - expenses; // Траты
             }
         }
         return count;
--- src/test/java/VacationCalculatorTest.java
+++ src/test/java/VacationCalculatorTest.java
@@ -1,5 +1,3 @@
-
-
 import org.junit.jupiter.api.Test;
 import ru.netology.services.VacationCalculator;
 
@@ -20,20 +18,5 @@ public class VacationCalculatorTest {
         VacationCalculator calculator = new VacationCalculator();
         int month2 = calculator.calculate(100000, 60000, 150000);
         assertEquals(2, month2);
-    }
-
-    @Test
-    public void testCalculate3() {
-        VacationCalculator calculator = new VacationCalculator();
-        int month3 = calculator.calculate(5000, 6000, 10000);
-        assertEquals(0, month3);
-    }
-
-    @Test
-    public void testCalculate4() {
-        VacationCalculator calculator = new VacationCalculator();
-        int month4 = calculator.calculate(50000, 10000, 100000);
-        assertEquals(4, month4);
-
     }
 }
